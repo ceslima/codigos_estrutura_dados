47dc9f9207d0fb10833eec30c1c5cdc51a49ea92
Carlos Lima <caddu.lima@hotmail.com>
2025-09-11 11:24:28 -0300

    Update grafico_simples.py

--- Introdução_matlibplot/grafico_simples.py
+++ Introdução_matlibplot/grafico_simples.py
@@ -1,1 +1,10 @@
+# Print the last item from year and pop
 
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year,pop)
+
+# Display the plot with plt.show()
+plt.show()
